eba5ab2c6270ac95b8c37f374b04936644869e0e
Amozon2k <159890919+Amozon2k@users.noreply.github.com>
2024-04-25 17:59:41 +0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,30 @@
 importScripts('https://www.gstatic.com/firebasejs/8.7.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.7.0/firebase-messaging.js');
-const firebaseConfig = {
-    apiKey: "AIzaSyDeVZ40U8FKl8jdIgllDo_fT5DstwCrmuM",
-    authDomain: "pwas-912f5.firebaseapp.com",
-    projectId: "pwas-912f5",
-    storageBucket: "pwas-912f5.appspot.com",
-    messagingSenderId: "718060089543",
-    appId: "1:718060089543:web:25168c10d0ab7e26920ee2",
-    measurementId: "G-NQ9QQ07510"
-  };
-  
-  firebase.initializeApp(firebaseConfig );
-  const messaging = firebase.messaging();
-  
-  messaging.onBackgroundMessage(function(payload) {
-      console.log('Получено уведомление:', payload);
-  
-      const notificationTitle = payload.notification.title;
-      const notificationOptions = {
-          body: payload.notification.body,
-          icon: payload.notification.icon ?? null
+(async function mama (){
+    const firebaseConfig = {
+        apiKey: "AIzaSyDeVZ40U8FKl8jdIgllDo_fT5DstwCrmuM",
+        authDomain: "pwas-912f5.firebaseapp.com",
+        projectId: "pwas-912f5",
+        storageBucket: "pwas-912f5.appspot.com",
+        messagingSenderId: "718060089543",
+        appId: "1:718060089543:web:25168c10d0ab7e26920ee2",
+        measurementId: "G-NQ9QQ07510"
       };
-  
-      self.registration.showNotification(notificationTitle, notificationOptions);
-  });
+      
+      firebase.initializeApp(firebaseConfig );
+      const messaging = firebase.messaging();
+      console.log('test')
+    
+        await messaging.requestPermission();
+        messaging.onBackgroundMessage(function(payload) {
+          console.log('Получено уведомление:', payload);
+      
+          const notificationTitle = payload.notification.title;
+          const notificationOptions = {
+              body: payload.notification.body,
+              icon: payload.notification.icon ?? null
+          };
+      
+          self.registration.showNotification(notificationTitle, notificationOptions);
+    });
+})();
