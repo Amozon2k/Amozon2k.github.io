9d58fa9efd57512910120b2f84a5b195484fad88
Amozon2k <159890919+Amozon2k@users.noreply.github.com>
2024-04-27 15:40:51 +0300

    Add files via upload

--- token.js
+++ token.js
@@ -1,4 +1,12 @@
-document.getElementById('tokenna').addEventListener("click",async ()=>{
-    document.getElementById('tokenn').innerText=token();
+// document.getElementById('tokenna').addEventListener("click",async ()=>{
+//     document.getElementById('tokenn').innerText=await messaging.getToken();;
+// });
+// 
+document.getElementById('tokenna').addEventListener("click", async () => {
+    try {
+        const token = await messaging.getToken();
+        document.getElementById('tokenn').innerText = token;
+    } catch (error) {
+        console.error('Ошибка при получении токена:', error);
+    }
 });
-// await messaging.getToken();
